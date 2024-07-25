5b5dd426d2e3c197f84e4af04c1b04c5587fb63b
lorena hortua <hortuaanyi@gmail.com>
2024-07-25 15:04:47 -0500

    cargando ejercicios de funciones

--- ejercicio4.php
+++ ejercicio4.php
@@ -1,1 +1,15 @@
-xgf nbfg 
+<?php
+function sumaDigitos($numero) {
+    $suma = 0;
+    $numero = abs($numero); 
+    while ($numero > 0) {
+        $suma += $numero % 10;
+        $numero = intval($numero / 10); 
+    }
+    return $suma;
+}
+
+$numero = 2213;
+$resultado = sumaDigitos($numero);
+echo "La suma de los dígitos de $numero es: $resultado";
+?>
